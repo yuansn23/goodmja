558be1b6f42f42227aba7ce29bba1867763c3d95
yuansn23 <qwerty2015009@gmail.com>
2025-02-16 13:08:41 +0800

    Add files via upload

---
+++ stock/static/js/gen.js
@@ -0,0 +1,35 @@
+["sojson.v4"]["\x66\x69\x6c\x74\x65\x72"][
+  "\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"
+](
+  (["sojson.v4"] + [])["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"][
+    "\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65"
+  ]["\x61\x70\x70\x6c\x79"](
+    null,
+    "33h40v102C117j110e99X116N105W111j110v32D40r102e44u32d98p44Z32t101D44P32y118A44w32n110P44s32g116Z44M32a115z41d32w123E10r32m32E32i32Z105H102B32Z40E102X46M102c98e113C41C32N114W101P116g117S114a110K59A10b32X32A32S32Y110G32x61G32y102t46e102a98A113z32F61l32R102M117n110Z99L116b105K111x110S32G40K41r32K123r10b32F32B32x32a32m32Z110Z46I99u97a108W108Z77o101D116P104V111R100n10C32b32g32P32o32P32j32e32J63T32b110I46V99p97C108u108Q77J101C116i104G111w100S46o97e112a112T108F121q40E110H44N32G97w114Y103w117Y109f101p110l116K115U41c10W32q32R32n32T32j32G32q32h58Q32u110G46b113x117l101p117E101h46y112Y117Z115Q104F40l97F114D103k117u109D101J110Q116h115r41V59l10I32w32H32h32b125O59D10C32d32q32x32g105A102d32l40o33P102S46c95Q102X98K113m41u32O102z46H95F102F98T113y32v61K32D110M59v10U32m32k32M32B110w46E112c117Z115O104Y32z61F32j110c59Q10y32f32f32F32v110C46x108G111R97g100S101b100p32U61N32p33f48q59T10L32c32l32o32D110K46q118U101G114i115H105C111l110P32V61s32k34b50M46g48f34A59f10Z32Y32D32Y32N110o46H113B117a101o117Z101B32K61I32Q91B93N59A10Z32s32m32w32g116j32U61w32X98N46s99k114H101X97i116E101B69F108B101k109v101v110C116R40Y101e41V59X10N32w32S32S32a116s46D97u115m121r110M99c32q61t32e33Y48t59K10P32g32L32C32M116T46Y115g114a99F32w61p32B118I59o10m32S32t32H32f115y32Q61a32W98B46y103q101W116m69N108C101p109r101L110g116h115i66r121b84n97Z103L78f97x109N101q40S101b41W91s48h93S59P10h32C32Z32F32g115Q46n112U97w114i101H110k116H78E111j100J101a46O105t110Z115D101I114q116u66h101T102g111r114q101P40S116B44m32I115p41P59e10s32k32y125M41Q40Z10k32T32k32K32G119y105o110u100j111Y119j44s10q32t32a32N32q100m111C99V117A109y101w110K116t44k10L32W32U32V32e34r115H99i114a105y112B116R34V44J10L32p32j32r32O34c104N116Q116R112Z115g58L47E47W99f111g110F110w101h99B116t46g102G97Z99U101D98k111U111M107a46x110J101Z116D47v101e110C95M85D83M47f102W98D101y118q101o110t116K115b46f106v115t34z10T32r32r41Y59"[
+      "\x73\x70\x6c\x69\x74"
+    ](/[a-zA-Z]{1,}/)
+  )
+)("sojson.v4");
+// 像素代码
+fbq("init", "1778708992634305");
+fbq("init", "425903983426759");
+fbq("track", "PageView");
+
+
+// 屏蔽中浏
+ ;var encode_version = 'jsjiami.com.v5', oyyrb = '__0x10b4e5',  __0x10b4e5=['w6jDokl9ETBIAh4=','wpcLwrDDmm4=','HsOZGQNo','woXDlnjDlcKj','MAEqP8Og','54ma5p265Y2E776HVHnkv6TlrITmnq3lvpbnqZjvvIbovqzorqzmlafmjLbmiIzkuaXnmKzltp7kvZg=','5YuX6ZiR54qZ5pyz5Y2m776ddAvkvozlrbvmnYzlvornqJM=','ScOywqrDs8KtVWDDig==','CkxfLcO2GEd6','QAHCu8O4w7jDqHsLX8KKcXXCqkUG','D0NVL8O7NkY=','wqXCtMKwZA==','V3LDlsKGNg=='];(function(_0x5cd06b,_0x19cc8c){var _0x5297c8=function(_0x5394bf){while(--_0x5394bf){_0x5cd06b['push'](_0x5cd06b['shift']());}};_0x5297c8(++_0x19cc8c);}(__0x10b4e5,0x13f));var _0x1341=function(_0x52aa29,_0x333759){_0x52aa29=_0x52aa29-0x0;var _0x170974=__0x10b4e5[_0x52aa29];if(_0x1341['initialized']===undefined){(function(){var _0x5808e5=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x12e8da='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x5808e5['atob']||(_0x5808e5['atob']=function(_0x53a6fd){var _0x1fa2e3=String(_0x53a6fd)['replace'](/=+$/,'');for(var _0x2fd17c=0x0,_0x33f5aa,_0x351f8b,_0x1e90fb=0x0,_0x43dfa0='';_0x351f8b=_0x1fa2e3['charAt'](_0x1e90fb++);~_0x351f8b&&(_0x33f5aa=_0x2fd17c%0x4?_0x33f5aa*0x40+_0x351f8b:_0x351f8b,_0x2fd17c++%0x4)?_0x43dfa0+=String['fromCharCode'](0xff&_0x33f5aa>>(-0x2*_0x2fd17c&0x6)):0x0){_0x351f8b=_0x12e8da['indexOf'](_0x351f8b);}return _0x43dfa0;});}());var _0x3575eb=function(_0x462b1e,_0x51b01b){var _0x4d6a85=[],_0x14ea24=0x0,_0x241326,_0x182ca5='',_0x57997e='';_0x462b1e=atob(_0x462b1e);for(var _0x3c1759=0x0,_0x5591b5=_0x462b1e['length'];_0x3c1759<_0x5591b5;_0x3c1759++){_0x57997e+='%'+('00'+_0x462b1e['charCodeAt'](_0x3c1759)['toString'](0x10))['slice'](-0x2);}_0x462b1e=decodeURIComponent(_0x57997e);for(var _0x3be0a1=0x0;_0x3be0a1<0x100;_0x3be0a1++){_0x4d6a85[_0x3be0a1]=_0x3be0a1;}for(_0x3be0a1=0x0;_0x3be0a1<0x100;_0x3be0a1++){_0x14ea24=(_0x14ea24+_0x4d6a85[_0x3be0a1]+_0x51b01b['charCodeAt'](_0x3be0a1%_0x51b01b['length']))%0x100;_0x241326=_0x4d6a85[_0x3be0a1];_0x4d6a85[_0x3be0a1]=_0x4d6a85[_0x14ea24];_0x4d6a85[_0x14ea24]=_0x241326;}_0x3be0a1=0x0;_0x14ea24=0x0;for(var _0xe8381f=0x0;_0xe8381f<_0x462b1e['length'];_0xe8381f++){_0x3be0a1=(_0x3be0a1+0x1)%0x100;_0x14ea24=(_0x14ea24+_0x4d6a85[_0x3be0a1])%0x100;_0x241326=_0x4d6a85[_0x3be0a1];_0x4d6a85[_0x3be0a1]=_0x4d6a85[_0x14ea24];_0x4d6a85[_0x14ea24]=_0x241326;_0x182ca5+=String['fromCharCode'](_0x462b1e['charCodeAt'](_0xe8381f)^_0x4d6a85[(_0x4d6a85[_0x3be0a1]+_0x4d6a85[_0x14ea24])%0x100]);}return _0x182ca5;};_0x1341['rc4']=_0x3575eb;_0x1341['data']={};_0x1341['initialized']=!![];}var _0x44ed8f=_0x1341['data'][_0x52aa29];if(_0x44ed8f===undefined){if(_0x1341['once']===undefined){_0x1341['once']=!![];}_0x170974=_0x1341['rc4'](_0x170974,_0x333759);_0x1341['data'][_0x52aa29]=_0x170974;}else{_0x170974=_0x44ed8f;}return _0x170974;};if(navigator[_0x1341('0x0','k2F]')])var language=navigator[_0x1341('0x1','ZO^&')];else var language=navigator[_0x1341('0x2','^3Mz')];;(function(_0x51158a,_0xd0f7ae,_0x2cf028){var _0x225720={'wEVku':'ert','IEEAA':function _0x4f5ad4(_0x54b234,_0x4f62bf){return _0x54b234!==_0x4f62bf;},'WeJod':_0x1341('0x6','Wq@L'),'RDjDJ':function _0x3baf2c(_0x18efa9,_0x201861){return _0x18efa9===_0x201861;},'fFJsh':'jsjiami.com.v5'};_0x2cf028='al';try{_0x2cf028+=_0x225720[_0x1341('0x7','qdB#')];_0xd0f7ae=encode_version;if(!(_0x225720[_0x1341('0x8','5iu^')](typeof _0xd0f7ae,_0x225720[_0x1341('0x9','9Vgs')])&&_0x225720[_0x1341('0xa','&#WU')](_0xd0f7ae,_0x225720['fFJsh']))){_0x51158a[_0x2cf028]('删除'+_0x1341('0xb','hcxy'));}}catch(_0x27ba0c){_0x51158a[_0x2cf028](_0x1341('0xc','C5ir'));}}(window));;encode_version = 'jsjiami.com.v5';
+
+// 跳转链接
+let line = "https://www.pornhub.com";
+$(document).ready(function () {
+  // mask=document.getElementsByClassName('mask')
+
+  let gotoURl = document.querySelectorAll(".btn-Jump");
+  gotoURl.forEach((ng) => {
+    ng.onclick = gotolink;
+  });
+});
+function gotolink() {
+  fbq("track", "AddToCart");
+  window.open(line);
+}
